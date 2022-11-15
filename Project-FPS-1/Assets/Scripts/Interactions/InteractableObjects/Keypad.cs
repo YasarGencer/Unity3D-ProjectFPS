7546eee3f25cd78728762d81d923dadf4fa32b32
@gncrysr <gncrysr@gmail.com>
2022-11-16 00:46:56 +0300

    New Interactables And Animations

--- Project-FPS-1/Assets/Scripts/Interactions/InteractableObjects/Keypad.cs
+++ Project-FPS-1/Assets/Scripts/Interactions/InteractableObjects/Keypad.cs
@@ -4,7 +4,36 @@ using UnityEngine;
 
 public class Keypad : Interactable
 {
+    public GameObject door;
+    [SerializeField] float animationTimer = 1f;
+    Animator doorAnim;
+    private bool isOpen = false, isInteractable = true;
+    private void Start() {
+        doorAnim = door.GetComponent<Animator>();
+    }
     protected override void Interact(){
-        Debug.Log("interact with = " + gameObject.name);
+        if(isInteractable){
+            if(isOpen)
+                CloseDoor();
+            else
+                OpenDoor();
+        }
+    }
+    private void OpenDoor(){
+        doorAnim.SetTrigger("Open");
+        isOpen = true;
+        StartCoroutine(SetUnInteractable("Close The Door"));
+    }
+    private void CloseDoor(){
+        doorAnim.SetTrigger("Close");
+        isOpen = false;
+        StartCoroutine(SetUnInteractable("Open The Door"));
+    }
+    IEnumerator SetUnInteractable(string prompt){
+        promptMessage = "Please Wait";
+        isInteractable = false;
+        yield return new WaitForSeconds(animationTimer);
+        promptMessage = prompt;
+        isInteractable = true;
     }
 }
