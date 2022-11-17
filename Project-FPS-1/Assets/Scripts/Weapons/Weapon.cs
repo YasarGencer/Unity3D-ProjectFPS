4c4412243db0335aa7e44a6133a777aff93560bb
@gncrysr <gncrysr@gmail.com>
2022-11-17 19:37:07 +0300

    Unused Librarys

---
+++ Project-FPS-1/Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1)]
+public class Weapon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
